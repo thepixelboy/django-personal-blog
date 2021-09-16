9ba3b7a4d34a60eeaa947e053a66cdeb9d02d53a
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-09-16 19:38:50 +0200

    feat: add urls and views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def starting_page(request):
+    pass
+
+
+def posts(request):
+    pass
+
+
+def single_post(request):
+    pass
