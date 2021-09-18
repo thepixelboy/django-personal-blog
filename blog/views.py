6bffd0649254ec8a1b5a36a903753f4e3f549fbd
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-09-18 12:45:57 +0200

    feat: add the single post page

--- blog/views.py
+++ blog/views.py
@@ -11,5 +11,5 @@ def posts(request):
     return render(request, "blog/all-posts.html")
 
 
-def single_post(request):
-    pass
+def single_post(request, slug):
+    return render(request, "blog/single-post.html")
