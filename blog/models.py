a284966ffab73aa08c54e09e5dcdfec9366446ef
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-09-26 16:15:41 +0200

    feat: add author model

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.core.validators import MinLengthValidator
 
 # Create your models here.
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email_address = models.EmailField()
+
+
 class Post(models.Model):
     title = models.CharField(max_length=150)
     excerpt = models.CharField(max_length=200)
@@ -9,3 +17,6 @@ class Post(models.Model):
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(
+        Author, on_delete=models.SET_NULL, related_name="posts"
+    )
