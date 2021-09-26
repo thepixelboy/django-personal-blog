53339ceaa82ffafa37d7fd05e5a4fe4dd50c2b7a
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-09-26 17:03:28 +0200

    feat: configure the admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 from .models import Author, Post, Tag
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_filter = (
+        "author",
+        "tags",
+        "date",
+    )
+    list_display = ("title", "date", "author")
+    prepopulated_fields = {"slug": ("title",)}
+
+
 admin.site.register(Author)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(Tag)
