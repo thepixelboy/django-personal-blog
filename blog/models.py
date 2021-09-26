98a98766ede5b6ec960d068f75c38695f896c104
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-09-26 16:18:26 +0200

    feat: add tag model

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,10 @@ from django.core.validators import MinLengthValidator
 # Create your models here.
 
 
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+
+
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
@@ -20,3 +24,4 @@ class Post(models.Model):
     author = models.ForeignKey(
         Author, on_delete=models.SET_NULL, related_name="posts"
     )
+    tags = models.ManyToManyField(Tag)
