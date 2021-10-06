591a773610a540a808b6599a282cd12afe2ece09
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-10-06 18:28:24 +0200

    feat: collect and serve static files

--- my_site/urls.py
+++ my_site/urls.py
@@ -18,7 +18,11 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("blog.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = (
+    [
+        path("admin/", admin.site.urls),
+        path("", include("blog.urls")),
+    ]
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+)
