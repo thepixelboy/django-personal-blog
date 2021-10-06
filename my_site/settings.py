c76de4b91ebc02133e3c91199da1f7f22f75060e
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-10-06 19:17:40 +0200

    feat: configure connection to a postgresql database server

--- my_site/settings.py
+++ my_site/settings.py
@@ -21,15 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-z-+j_fwo1h00o(bs7yzluoug2y*34ti1obuy9yj_2$7q7fiv+v"
-)
+SECRET_KEY = getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = getenv("IS_DEVELOPMENT", True)
 
 ALLOWED_HOSTS = [
-    getenv("APP_HOST"),
+    getenv("APP_HOST", "localhost"),
 ]
 
 
@@ -80,9 +78,17 @@ WSGI_APPLICATION = "my_site.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": getenv("DB_USER_NAME"),
+        "PASSWORD": getenv("DB_USER_PASSWORD"),
+        "HOST": getenv("DB_HOST"),
+        "PORT": getenv("DB_PORT"),
     }
 }
 
